1f1d058c42ac771c80c7c709af8b9bd90d79d663
Abbass Hassan <abbasshassan@Abbasss-MacBook-Air.local>
2025-04-28 17:43:34 +0300

    Add factory for Comment model

--- backend/database/factories/CommentFactory.php
+++ backend/database/factories/CommentFactory.php
@@ -2,22 +2,33 @@
 
 namespace Database\Factories;
 
+use App\Models\Comment;
+use App\Models\User;
+use App\Models\Video;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
- */
 class CommentFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
+    protected $model = Comment::class;
+
     public function definition(): array
     {
         return [
-            //
+            'user_id' => User::factory(),
+            'video_id' => Video::factory(),
+            'content' => $this->faker->paragraph(),
+            'parent_id' => null,
+            'created_at' => $this->faker->dateTimeBetween('-6 months', 'now'),
+            'updated_at' => now(),
         ];
+    }
+
+    public function reply(): static
+    {
+        return $this->state(function (array $attributes) {
+            return [
+                'parent_id' => Comment::factory(),
+            ];
+        });
     }
 }
