bfa971dd1c6deaf50e6f4ef6dc63460784f2e37b
Abbass Hassan <abbasshassan@Abbasss-MacBook-Air.local>
2025-05-04 02:01:19 +0300

    create VideoDiscoveryController structure

--- backend/app/Http/Controllers/API/regular/VideoDiscoveryController.php
+++ backend/app/Http/Controllers/API/regular/VideoDiscoveryController.php
@@ -1,11 +1,14 @@
 <?php
 
-namespace App\Http\Controllers\Api\Regular;
+namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Video;
 use App\Services\VideoService;
-use App\Services\FollowService;
 use App\Services\InvestmentService;
+use App\Services\FollowService;
+use App\Services\UserService;
+use App\Services\SearchService;
 use App\Traits\ApiResponse;
 use Illuminate\Http\Request;
 
@@ -14,16 +17,23 @@ class VideoDiscoveryController extends Controller
     use ApiResponse;
     
     protected $videoService;
-    protected $followService;
     protected $investmentService;
+    protected $followService;
+    protected $userService;
+    protected $searchService;
     
     public function __construct(
         VideoService $videoService,
+        InvestmentService $investmentService,
         FollowService $followService,
-        InvestmentService $investmentService
+        UserService $userService,
+        SearchService $searchService
     ) {
         $this->videoService = $videoService;
-        $this->followService = $followService;
         $this->investmentService = $investmentService;
+        $this->followService = $followService;
+        $this->userService = $userService;
+        $this->searchService = $searchService;
     }
+    
 }
