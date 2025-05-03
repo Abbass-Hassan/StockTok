3f24e159eb21f3c97c02312fb9b63269365e9ac9
Abbass Hassan <abbasshassan@Abbasss-MacBook-Air.local>
2025-05-03 15:28:05 +0300

    add register method to AuthController

--- backend/app/Http/Controllers/API/AuthController.php
+++ backend/app/Http/Controllers/API/AuthController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Services\AuthService;
 use App\Traits\ApiResponse;
+use App\Http\Requests\RegisterRequest;
 
 class AuthController extends Controller
 {
@@ -15,5 +16,26 @@ class AuthController extends Controller
     public function __construct(AuthService $authService)
     {
         $this->authService = $authService;
+    }
+
+
+    /**
+     * Register a new user.
+     *
+     * @param RegisterRequest $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function register(RegisterRequest $request)
+    {
+        // Use validated data from the request
+        $user = $this->authService->register($request->validated());
+        
+        // Generate token
+        $token = $user->createToken('auth_token')->plainTextToken;
+        
+        return $this->successResponse([
+            'user' => $user,
+            'token' => $token
+        ], 'User registered successfully', 201);
     }
 }
