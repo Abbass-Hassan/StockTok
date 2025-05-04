911f34071363879efb3b773bae88d607fcaa049e
Abbass Hassan <abbasshassan@Abbasss-MacBook-Air.local>
2025-05-04 03:20:37 +0300

    create basic InvestmentController structure

---
+++ backend/app/Http/Controllers/API/regular/InvestmentController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers\Api\Regular;
+
+use App\Http\Controllers\Controller;
+use App\Services\InvestmentService;
+use App\Services\WalletService;
+use App\Services\VideoService;
+use App\Traits\ApiResponse;
+use Illuminate\Http\Request;
+
+class InvestmentController extends Controller
+{
+    use ApiResponse;
+    
+    protected $investmentService;
+    protected $walletService;
+    protected $videoService;
+    
+    public function __construct(
+        InvestmentService $investmentService,
+        WalletService $walletService,
+        VideoService $videoService
+    ) {
+        $this->investmentService = $investmentService;
+        $this->walletService = $walletService;
+        $this->videoService = $videoService;
+    }
+    
+    // Methods will go here
+}
