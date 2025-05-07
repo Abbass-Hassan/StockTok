319d2b0255e399f9326a6a1ab9a2c9765e89577a
Abbass Hassan <abbasshassan@Abbasss-MacBook-Air.local>
2025-05-08 00:10:19 +0300

    Create basic AuthLayout component structure

---
+++ frontend/src/components/specific/Auth/AuthLayout.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {View, StyleSheet} from 'react-native';
+
+const AuthLayout = ({header, form, socialSection, bottomPrompt}) => {
+  return <View>{/* Placeholder for content */}</View>;
+};
+
+const styles = StyleSheet.create({});
+
+export default AuthLayout;
