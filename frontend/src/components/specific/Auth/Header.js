b71be89eef9bc04d97290798180c37e553771a1d
Abbass Hassan <abbasshassan@Abbasss-MacBook-Air.local>
2025-05-08 00:11:24 +0300

    Create basic Header component structure

---
+++ frontend/src/components/specific/Auth/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+const Header = ({title, subtitle}) => {
+  return <View style={styles.container}>{/* Placeholder content */}</View>;
+};
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+  },
+});
+
+export default Header;
