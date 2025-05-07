a50eb3594da76decb9e44a919bc1f425cb175488
Abbass Hassan <abbasshassan@Abbasss-MacBook-Air.local>
2025-05-08 01:32:18 +0300

    Add Register screen to navigation stack

--- frontend/src/navigation/index.js
+++ frontend/src/navigation/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import Login from '../screens/Auth/Login';
+import Register from '../screens/Auth/Register';
+import ForgotPassword from '../screens/Auth/ForgotPassword';
 import HomeScreen from '../screens/Feed';
 import UploadVideo from '../screens/Creator/UploadVideo';
 import MyVideos from '../screens/Creator/MyVideos';
@@ -12,24 +14,27 @@ const Stack = createStackNavigator();
 const Navigation = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Login">
-        <Stack.Screen
-          name="Login"
-          component={Login}
-          options={{headerShown: false}}
-        />
+      <Stack.Navigator
+        initialRouteName="Login"
+        screenOptions={{headerShown: false}}>
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Register" component={Register} />
+        <Stack.Screen name="ForgotPassword" component={ForgotPassword} />
+
         <Stack.Screen
           name="Home"
           component={HomeScreen}
           options={{
+            headerShown: true,
             title: 'StockTok',
-            headerBackVisible: false, // Prevents going back to login
+            headerBackVisible: false,
           }}
         />
         <Stack.Screen
           name="UploadVideo"
           component={UploadVideo}
           options={{
+            headerShown: true,
             title: 'Upload Video',
           }}
         />
@@ -37,6 +42,7 @@ const Navigation = () => {
           name="MyVideos"
           component={MyVideos}
           options={{
+            headerShown: true,
             title: 'My Videos',
           }}
         />
@@ -44,6 +50,7 @@ const Navigation = () => {
           name="VideoPlayer"
           component={VideoPlayer}
           options={{
+            headerShown: true,
             title: 'Video',
             headerBackTitle: 'Back',
           }}
