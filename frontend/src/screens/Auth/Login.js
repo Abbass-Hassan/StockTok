44fc650a883f7796780865780323a97826b4073e
Abbass Hassan <abbasshassan@Abbasss-MacBook-Air.local>
2025-05-08 00:19:01 +0300

    Create basic Login screen file

---
+++ frontend/src/screens/Auth/Login.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import {Alert} from 'react-native';
+
+// Will add imports and component structure later
+const Login = ({navigation}) => {
+  return null; // Placeholder return
+};
+
+export default Login;
