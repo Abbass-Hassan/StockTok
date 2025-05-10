dd566a2303f2587cabc92e3968db573b09d1a803
Abbass Hassan <abbasshassan@Abbasss-MacBook-Air.local>
2025-05-11 01:43:06 +0300

    Import auth, feed, and creator screens

--- frontend/src/navigation/index.js
+++ frontend/src/navigation/index.js
@@ -1,5 +1,12 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
+import Login from '../screens/Auth/Login';
+import Register from '../screens/Auth/Register';
+import ProfileCompletion from '../screens/Auth/ProfileCompletion';
+import HomeScreen from '../screens/Feed';
+import UploadVideo from '../screens/Creator/UploadVideo';
+import MyVideos from '../screens/Creator/MyVideos';
+import VideoPlayer from '../screens/Creator/VideoPlayer';
 
 const Stack = createStackNavigator();
