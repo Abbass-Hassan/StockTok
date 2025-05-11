17fb25f409eb877423a4a1919433e95b86bd9d86
Abbass Hassan <abbasshassan@Abbasss-MacBook-Air.local>
2025-05-11 12:12:44 +0300

    Import auth-related screens: Login, Register, ProfileCompletion

--- frontend/src/navigation/index.js
+++ frontend/src/navigation/index.js
@@ -1,3 +1,6 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
+import Login from '../screens/Auth/Login';
+import Register from '../screens/Auth/Register';
+import ProfileCompletion from '../screens/Auth/ProfileCompletion';
