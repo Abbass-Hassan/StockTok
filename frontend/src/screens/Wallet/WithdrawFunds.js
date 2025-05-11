ff96892a4643b9c91579e852717401d70b46112e
Abbass Hassan <abbasshassan@Abbasss-MacBook-Air.local>
2025-05-12 01:17:45 +0300

    Import React and core React Native components for WithdrawFunds screen

---
+++ frontend/src/screens/Wallet/WithdrawFunds.js
@@ -0,0 +1,14 @@
+import React, {useState, useEffect} from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  SafeAreaView,
+  StatusBar,
+  Platform,
+  TextInput,
+  Alert,
+  ActivityIndicator,
+  ScrollView,
+} from 'react-native';
