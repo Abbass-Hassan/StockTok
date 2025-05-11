bd393ae5ca39e88e67fccc6c64bf563357e56932
Abbass Hassan <abbasshassan@Abbasss-MacBook-Air.local>
2025-05-11 12:12:52 +0300

    Import main feed and creator-related screens

--- frontend/src/navigation/index.js
+++ frontend/src/navigation/index.js
@@ -4,3 +4,8 @@ import {createStackNavigator} from '@react-navigation/stack';
 import Login from '../screens/Auth/Login';
 import Register from '../screens/Auth/Register';
 import ProfileCompletion from '../screens/Auth/ProfileCompletion';
+import HomeScreen from '../screens/Feed';
+import UploadVideo from '../screens/Creator/UploadVideo';
+import MyVideos from '../screens/Creator/MyVideos';
+import VideoPlayer from '../screens/Creator/VideoPlayer';
+import VideoDetails from '../screens/Creator/VideoDetails';
