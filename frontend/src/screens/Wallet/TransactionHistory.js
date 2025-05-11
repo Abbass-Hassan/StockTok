779d35ead9689c48c9cdd1ce14032f62e06baa93
Abbass Hassan <abbasshassan@Abbasss-MacBook-Air.local>
2025-05-12 01:09:35 +0300

    Import React and React Native components for TransactionHistory

---
+++ frontend/src/screens/Wallet/TransactionHistory.js
@@ -0,0 +1,13 @@
+import React, {useState, useEffect} from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  SafeAreaView,
+  StatusBar,
+  Platform,
+  FlatList,
+  ActivityIndicator,
+  RefreshControl,
+} from 'react-native';
