056b42911d5b1a04725af6d4a5d2a60b9a7f75a1
Abbass Hassan <abbasshassan@Abbasss-MacBook-Air.local>
2025-05-12 23:12:39 +0300

    chore: import core react-native UI components

--- frontend/src/screens/Regular/Search.js
+++ frontend/src/screens/Regular/Search.js
@@ -1,1 +1,13 @@
 import React, {useState, useEffect} from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  FlatList,
+  TouchableOpacity,
+  Image,
+  ActivityIndicator,
+  SafeAreaView,
+  StatusBar,
+} from 'react-native';
