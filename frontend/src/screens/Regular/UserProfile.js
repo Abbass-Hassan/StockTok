aab2a08783269d87c7bd0a9180be4e2c415216c2
Abbass Hassan <abbasshassan@Abbasss-MacBook-Air.local>
2025-05-12 23:18:05 +0300

    chore: import core React Native UI components

--- frontend/src/screens/Regular/UserProfile.js
+++ frontend/src/screens/Regular/UserProfile.js
@@ -1,1 +1,14 @@
 import React, {useState, useEffect} from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  TouchableOpacity,
+  FlatList,
+  ActivityIndicator,
+  SafeAreaView,
+  StatusBar,
+  Dimensions,
+  Alert,
+} from 'react-native';
