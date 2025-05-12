c5024500fae003d56fab942b1e3bac784d53e5e3
Abbass Hassan <abbasshassan@Abbasss-MacBook-Air.local>
2025-05-12 10:55:25 +0300

    Add number formatter for stats display

--- frontend/src/screens/Creator/CreatorProfile.js
+++ frontend/src/screens/Creator/CreatorProfile.js
@@ -56,4 +56,9 @@ const CreatorProfile = ({navigation}) => {
         setRefreshing(true);
         loadProfileData();
       };
+      const formatNumber = num => {
+        if (num >= 1000000) return `${(num / 1000000).toFixed(1)}M`;
+        if (num >= 1000) return `${(num / 1000).toFixed(1)}K`;
+        return num.toString();
+      };
     
