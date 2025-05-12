c64492964c509aa66e13fefa15933fbe1288037d
Abbass Hassan <abbasshassan@Abbasss-MacBook-Air.local>
2025-05-13 02:53:59 +0300

    chore: import core React Native components

--- frontend/src/screens/VideoFeed/VideoFeedScreen.js
+++ frontend/src/screens/VideoFeed/VideoFeedScreen.js
@@ -1,1 +1,13 @@
 import React, {useState, useEffect, useRef} from 'react';
+import {
+  View,
+  FlatList,
+  StyleSheet,
+  ActivityIndicator,
+  Text,
+  Image,
+  Dimensions,
+  TouchableOpacity,
+  StatusBar,
+  Platform,
+} from 'react-native';
