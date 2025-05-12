0b139f1789974093a89032c25a2e04fe7ca3db5a
Abbass Hassan <abbasshassan@Abbasss-MacBook-Air.local>
2025-05-13 02:54:14 +0300

    chore: define API_URL and device dimensions

--- frontend/src/screens/VideoFeed/VideoFeedScreen.js
+++ frontend/src/screens/VideoFeed/VideoFeedScreen.js
@@ -14,3 +14,5 @@ import {
 import Video from 'react-native-video';
 import axios from 'axios';
 import {getToken} from '../../utils/tokenStorage';
+const API_URL = 'http://13.37.224.245:8000/api';
+const {height, width} = Dimensions.get('window');
