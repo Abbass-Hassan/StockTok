7a83f131c8553e7cf8221eab351e4b0b0bb57148
Abbass Hassan <abbasshassan@Abbasss-MacBook-Air.local>
2025-05-12 23:26:44 +0300

    chore: set up Regular screen with imports and initial state

---
+++ frontend/src/screens/Feed/Regular.js
@@ -0,0 +1,14 @@
+import React, {useState, useEffect} from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  SafeAreaView,
+  StatusBar,
+  TouchableOpacity,
+  Alert,
+} from 'react-native';
+import {getUserData} from '../../utils/tokenStorage';
+
+const Regular = ({navigation}) => {
+  const [userData, setUserData] = useState(null);
