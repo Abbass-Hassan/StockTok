5a769993d60b6ed4345c4cf3aa717e0aec846a48
Abbass Hassan <abbasshassan@Abbasss-MacBook-Air.local>
2025-05-13 02:54:21 +0300

    chore: calculate available screen height for video

--- frontend/src/screens/VideoFeed/VideoFeedScreen.js
+++ frontend/src/screens/VideoFeed/VideoFeedScreen.js
@@ -16,3 +16,8 @@ import axios from 'axios';
 import {getToken} from '../../utils/tokenStorage';
 const API_URL = 'http://13.37.224.245:8000/api';
 const {height, width} = Dimensions.get('window');
+const TAB_BAR_HEIGHT = 80;
+const BOTTOM_INSET = Platform.OS === 'ios' ? 34 : 16;
+const STATUS_BAR_HEIGHT = StatusBar.currentHeight || 0;
+const AVAILABLE_HEIGHT =
+  height - TAB_BAR_HEIGHT - BOTTOM_INSET - STATUS_BAR_HEIGHT;
