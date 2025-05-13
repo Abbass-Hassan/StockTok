bfcbc6588a284d62607e782aa4ab8937fa4235c9
Abbass Hassan <abbasshassan@Abbasss-MacBook-Air.local>
2025-05-13 19:17:59 +0300

    2. Import API modules, constants, and icons

--- frontend/src/screens/Investment/PortfolioScreen.js
+++ frontend/src/screens/Investment/PortfolioScreen.js
@@ -14,3 +14,10 @@ import {
   SafeAreaView,
   Platform,
 } from 'react-native';
+import {investmentApi} from '../../api/investmentApi';
+import {getToken} from '../../utils/tokenStorage';
+import Icon from 'react-native-vector-icons/Ionicons';
+import axios from 'axios';
+
+const API_URL = 'http://13.37.224.245:8000/api';
+const {width} = Dimensions.get('window');
