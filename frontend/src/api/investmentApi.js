90dd8a84abce136d830e833b3181919eb6ef360b
Abbass Hassan <abbasshassan@Abbasss-MacBook-Air.local>
2025-05-13 19:09:10 +0300

    Start investmentApi object

--- frontend/src/api/investmentApi.js
+++ frontend/src/api/investmentApi.js
@@ -2,3 +2,4 @@ import axios from 'axios';
 import {getToken} from '../utils/tokenStorage'; // Make sure this path is correct
 
 const API_URL = 'http://13.37.224.245:8000/api';
+export const investmentApi = {
