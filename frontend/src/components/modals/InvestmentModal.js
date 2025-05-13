0d0a9017cdf7ad98f14f1d3aa153203fb614549d
Abbass Hassan <abbasshassan@Abbasss-MacBook-Air.local>
2025-05-13 19:13:33 +0300

    Create InvestmentModal component with required imports

---
+++ frontend/src/components/modals/InvestmentModal.js
@@ -0,0 +1,18 @@
+import React, {useState} from 'react';
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  StyleSheet,
+  Modal,
+  ActivityIndicator,
+  TouchableWithoutFeedback,
+  Keyboard,
+  Alert,
+  Vibration,
+} from 'react-native';
+import investmentApi from '../../api/investmentApi';
+import Icon from 'react-native-vector-icons/Ionicons';
+
+const InvestmentModal = ({visible, videoId, onClose, onSuccess}) => {
