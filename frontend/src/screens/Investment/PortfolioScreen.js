47b3bc1c86e0f7685e837886beaab4071b404fdd
Abbass Hassan <abbasshassan@Abbasss-MacBook-Air.local>
2025-05-13 19:17:51 +0300

    1. Import core React Native components

---
+++ frontend/src/screens/Investment/PortfolioScreen.js
@@ -0,0 +1,16 @@
+import React, {useState, useEffect} from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  FlatList,
+  TouchableOpacity,
+  Image,
+  ScrollView,
+  RefreshControl,
+  ActivityIndicator,
+  Dimensions,
+  StatusBar,
+  SafeAreaView,
+  Platform,
+} from 'react-native';
